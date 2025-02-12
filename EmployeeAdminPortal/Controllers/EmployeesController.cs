c512725c389f7a09da400f1ad13fa368b44acf4e
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-12 10:12:34 +0100

    DeleteEmployee - method

--- EmployeeAdminPortal/Controllers/EmployeesController.cs
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -79,5 +79,23 @@ namespace EmployeeAdminPortal.Controllers
 
             return Ok(employee);
         }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+
+        public IActionResult DeleteEmployee(Guid id) 
+        {
+            var employee = dbContext.Employees.Find(id);
+
+            if (employee is null)
+            {
+                return NotFound();
+            }
+
+            dbContext.Employees.Remove(employee);
+            dbContext.SaveChanges();
+
+            return Ok(); 
+        }
     }
 }
